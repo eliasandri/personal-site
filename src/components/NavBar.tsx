e12de05b7e26b8c321ec5bbd21ebf6f4ce998a62
Elias Andri <eliasandriasgeirsson@gmail.com>
2025-01-21 22:58:05 +0000

    Build the color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,13 @@
-import { HStack, Text } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 import mexico from "../assets/ESCUDO_MEXICO-logo-B3E591388D-seeklogo.com.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
-      <Image src={mexico} boxSize="60px" p={2}></Image>
-      <Text>NavBar</Text>
+    <HStack justifyContent="space-between" padding="10px">
+      <Image src={mexico} boxSize="60px"></Image>
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
 };
